97d228fddcf3a0c59d842c6add1fc9acae227721
Henrique Hemerly Maia <yyz.hhm@gmail.com>
2021-04-16 20:36:06 -0300

    some corrections

--- preprocessing.py
+++ preprocessing.py
@@ -5,19 +5,22 @@ Augment images and save them as a tfrecord.
 
 import os
 import argparse
+import matplotlib.pyplot as plt
 import tensorflow as tf 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 _RANDOM_SEED = 983437
 
-
 def iterator_from_directory(input_dir, batch_size=32, output_dir=None):
+    '''
+    Generates augmented images using Keras preprocessing module.
+    '''
     image_generator = ImageDataGenerator(
             rotation_range=36,
             width_shift_range=0.2,
             height_shift_range=0.2,
-            shear_range=36,
+            #shear_range=36,
             zoom_range=0.1,
             channel_shift_range=0.1,
             fill_mode='reflect',
@@ -37,38 +40,49 @@ def iterator_from_directory(input_dir, batch_size=32, output_dir=None):
 
 
 def image_example(image_raw, label):
+    '''
+    Creates a tensorflow.train.Example from an image and a label.
+    '''
+     # Helper functions for data type conversions before serialization  
     def _int64_feature(value):
         return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
     def _bytes_feature(value):
         return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-    feature = {
-        'label': _int64_feature(int(label)),
-        'image_raw': _bytes_feature(tf.io.encode_jpeg(image_raw).numpy())
-    }
 
+   feature = {
+            'label': _int64_feature(int(label)),
+            'image_raw': _bytes_feature(tf.io.encode_jpeg(image_raw).numpy())
+    }
     return tf.train.Example(features=tf.train.Features(feature=feature))
 
 
-def write_tfrecord(record_fname, iterator, n_batches):
+def write_augmented_images_tfrecord(record_fname, iterator, n_batches):
+    '''
+    Given an iterator that yields tuples of iterables (batches) of images and 
+    labels, writes a tfrecord from it. 
+    '''
     with tf.io.TFRecordWriter(record_fname) as writer:
         for batch_id in range(n_batches):
             print('\n:: Processing batch #{}...'.format(batch_id))
             batch = iterator.next()
-            i = 1
+
+            i = 0
             for image_raw, label in zip(*batch): 
-                tf_example = image_example(image_raw, label)
-                writer.write(tf_example.SerializeToString())
-                print('\r  |_ file #{}.'.format(i), end='')
+                example = image_example(image_raw, label)
+                writer.write(example.SerializeToString())
+
                 i = i + 1
+                print('\r  |_ #{} images processed.'.format(i), end='')
 
 
-def dataset_from_tfrecord(record_path):
+def augmented_images_dataset_from_tfrecord(record_path):
+    # function for decoding a dataset saved in tfrecord format
     def decode_example(example):
         schema = {
-            'label': tf.io.FixedLenFeature([], dtype=tf.int64),
-            'image_raw': tf.io.FixedLenFeature([], dtype=tf.string)
+                'label': tf.io.FixedLenFeature([], dtype=tf.int64),
+                'image_raw': tf.io.FixedLenFeature([], dtype=tf.string)
         }
         example = tf.io.parse_single_example(example, schema)
         example['image'] = tf.io.decode_jpeg(example.pop('image_raw'))
@@ -82,17 +96,17 @@ def dataset_from_tfrecord(record_path):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
             description="Preprocess desafio dataset, applying augmentations"
-                        " and saving it inside a tfrecord."
-    parser.add_argument('-i', type=str
+                        " and saving it inside a tfrecord.")
+    parser.add_argument('-i', type=str,
             help='Path to the root of desafio dataset.')
-    parser.add_argument('-o', type=str
-            help='Director where to save the splits.')
-    parser.add_argument('-b', type=int
+    parser.add_argument('-o', type=str,
+            help='Name of the output tfrecord')
+    parser.add_argument('-b', type=int,
             help='Batch size for ImageDataGenerator.')
-    parser.add_argument('-n', type=int
+    parser.add_argument('-n', type=int,
             help='Number of batches to process.')
     args = parser.parse_args()
 
     it = iterator_from_directory(args.i, args.b)
-    write_tfrecord(args.o, it, args.n)
+    write_augmented_images_tfrecord(args.o, it, args.n)
 
