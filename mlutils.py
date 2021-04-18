1865264d9bb70f4f857668a049d932fa97de67b5
Henrique Hemerly Maia <yyz.hhm@gmail.com>
2021-04-18 16:34:49 -0300

    removing utils.py in favor of mlutils.py

--- mlutils.py
+++ mlutils.py
@@ -52,6 +52,30 @@ def export_vocabulary(path, vocab_size, word_index):
         f.write('\n'.join(words[:vocab_size]))
 
 
+def plot_augmented_images_dataset_samples(
+        dataset, fig_index=0, buffer_size=10000):
+
+    dataset.shuffle(buffer_size)
+    samples = dataset.take(6)
+
+    i = 1
+    fig = plt.figure(fig_index)
+    for example in samples.as_numpy_iterator():
+        subp = plt.subplot(2, 3, i) 
+        subp.imshow(example['image'])
+        subp.set_title(str(example['label']))
+        i = i+1
+    plt.show()
+    
+
+def plot_backbone_featuremap_samples(features, fig_index=0):
+    fig = plt.figure(fig_index)
+    for i in range(1, 7):
+        subp = plt.subplot(2, 3, i) 
+        subp.imshow(features[0,:,:,i])
+    plt.show()
+ 
+
 def plot_series(x, y, scale='log'):
     fig = plt.figure()
     sub = fig.add_subplot()
